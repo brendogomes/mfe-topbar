19dd9fb48a770faf231facd9a184bf93a3d9b9ca
educbank <brendo.gomes@educbank.com.br>
2024-07-28 15:46:13 -0300

    remove: removendo router module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -18,7 +16,6 @@ import { MatMenuModule } from '@angular/material/menu';
   imports: [ 
     BrowserModule,
     BrowserAnimationsModule,
-    AppRoutingModule,
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
